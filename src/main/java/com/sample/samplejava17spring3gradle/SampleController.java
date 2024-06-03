e54adbb9d0401609396ec91375c0daed30353e21
이서린/서비스개발팀/EP <seolin.lee@nhn.com>
2024-06-03 10:57:57 +0900

    feat: sleep 1초 걸어서 응답 지연

--- src/main/java/com/sample/samplejava17spring3gradle/SampleController.java
+++ src/main/java/com/sample/samplejava17spring3gradle/SampleController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
     @GetMapping("/")
-    public String sample() {
+    public String sample() throws InterruptedException {
+        Thread.sleep(1000);
         return "success";
     }
 
